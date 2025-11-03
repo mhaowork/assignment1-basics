9078a4206acc6150e48579831efd38554bb3154a
mhaowork <mhaowork@users.noreply.github.com>
2025-11-02 20:27:13 -0800

    impl swiglu

---
+++ cs336_basics/swiglu.py
@@ -0,0 +1,27 @@
+from jaxtyping import Float
+import torch
+from torch import Tensor, nn
+
+class SwiGLU(nn.Module):
+  def __init__(self, d_model: int, d_ff: int):
+    super().__init__()
+
+    self.d_model = d_model
+    self.d_ff = d_ff
+
+    self.w1 = nn.Parameter(torch.empty(d_ff, d_model))
+    self.w2 = nn.Parameter(torch.empty(d_model, d_ff))
+    self.w3 = nn.Parameter(torch.empty(d_ff, d_model))
+
+  def forward(self, x: Float[Tensor, "... d_model"]) -> Float[Tensor, "... d_model"]:
+    # SiLU(x) = x·σ(x) = x / (1 + e ** −x)
+    # FFN(x) = SwiGLU(x, W1, W2, W3) = (SiLU(xW1) ⊙ xW3)w2
+
+    xw1: Float[Tensor, "... d_ff"] = x @ self.w1.T
+    xw3: Float[Tensor, "... d_ff"] = x @ self.w3.T
+    silu: Float[Tensor, "... d_ff"] = xw1 * torch.sigmoid(xw1)
+
+
+    return (silu * xw3) @ self.w2.T
+    
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -12,6 +12,7 @@ from torch import Tensor
 from cs336_basics.embedding import Embedding
 from cs336_basics.linear import Linear
 from cs336_basics.rmsnorm import RMSNorm
+from cs336_basics.swiglu import SwiGLU
 
 
 def run_linear(
@@ -93,7 +94,11 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+
+    swiglu = SwiGLU(d_model, d_ff)
+
+    swiglu.load_state_dict({'w1': w1_weight, 'w2': w2_weight, 'w3': w3_weight})
+    return swiglu.forward(in_features)
 
 
 def run_scaled_dot_product_attention(
